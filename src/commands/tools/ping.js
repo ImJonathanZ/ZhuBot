327bab30fdbaf0f2b142b8a22f6c4411b9da8f49
Jonathan Zhu <jzhu257@gmail.com>
2023-05-17 17:47:55 -0400

    fixed ping message formatting

--- src/commands/tools/ping.js
+++ src/commands/tools/ping.js
@@ -7,9 +7,7 @@ module.exports = {
     async execute(interaction, client) {
         const message = await interaction.deferReply({ fetchReply: true });
 
-        const newMessage = `Latency is
-        ${message.createdTimestamp - interaction.createdTimestamp}ms.
-        \nAPI Latency is ${Math.round(client.ws.ping)}ms.`;
+        const newMessage = `Latency is ${message.createdTimestamp - interaction.createdTimestamp}ms.\nAPI Latency is ${Math.round(client.ws.ping)}ms.`;
         await interaction.editReply({ content: newMessage });
     },
 };
